a9935ce4c43fd5862236b8fce07df9e2018318d6
kali <kali@kali.kali>
2023-07-02 09:30:36 -0500

    form validation

---
+++ src/components/Forms.jsx
@@ -0,0 +1,171 @@
+import { useState } from "react";
+import useHttp from "../hooks/useHttp";
+
+const Forms = () => {
+    const { request } = useHttp();
+
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleName = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handelPassword = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (password.length < 8) {
+      return (
+        setMessage("Password invalid"),
+        setTimeout(() => {
+          setMessage("");
+        }, 2500)
+      );
+    }
+    const re =
+      /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+
+    if (!re.test(email)) {
+      return (
+        setMessage("invalid email"),
+        setTimeout(() => {
+          setMessage("");
+        }, 2500)
+      );
+    }
+
+    const newUsers = {
+      name,
+      password,
+      email,
+    };
+
+    request(
+      "http://localhost:3001/users",
+      "POST",
+      JSON.stringify(newUsers)
+    ).then(
+      alert("seccessfull validation "),
+
+      setName(""),
+      setPassword(""),
+      setEmail("")
+    );
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <span></span> <span></span> <span></span> <span></span> <span></span>
+      <span></span> <span></span> <span></span> <span></span> <span></span>
+      <span></span> <span></span> <span></span> <span></span> <span></span>
+      <span></span> <span></span> <span></span> <span></span> <span></span>
+      <span></span> <span></span> <span></span> <span></span> <span></span>
+      <span></span> <span></span> <span></span> <span></span> <span></span>
+      <span></span> <span></span> <span></span> <span></span> <span></span>
+      <span></span> <span></span> <span></span> <span></span> <span></span>
+      <span></span> <span></span> <span></span> <span></span> <span></span>
+      <span></span> <span></span> <span></span> <span></span> <span></span>
+      <span></span> <span></span> <span></span> <span></span> <span></span>
+      <span></span> <span></span> <span></span> <span></span> <span></span>
+      <span></span> <span></span> <span></span> <span></span> <span></span>
+      <span></span> <span></span> <span></span> <span></span> <span></span>
+      <span></span> <span></span> <span></span> <span></span> <span></span>
+      <span></span> <span></span> <span></span> <span></span> <span></span>
+      <span></span> <span></span> <span></span> <span></span> <span></span>
+      <span></span> <span></span> <span></span> <span></span> <span></span>
+      <span></span> <span></span> <span></span> <span></span> <span></span>
+      <span></span> <span></span> <span></span> <span></span> <span></span>
+      <span></span> <span></span> <span></span> <span></span> <span></span>
+      <span></span> <span></span> <span></span> <span></span> <span></span>
+      <span></span> <span></span> <span></span> <span></span> <span></span>
+      <span></span> <span></span> <span></span> <span></span> <span></span>
+      <span></span> <span></span> <span></span> <span></span> <span></span>
+      <span></span> <span></span> <span></span> <span></span> <span></span>
+      <span></span> <span></span> <span></span> <span></span> <span></span>
+      <span></span> <span></span> <span></span> <span></span> <span></span>
+      <span></span> <span></span> <span></span> <span></span> <span></span>
+      <span></span> <span></span> <span></span> <span></span> <span></span>
+      <span></span> <span></span> <span></span> <span></span> <span></span>
+      <span></span> <span></span> <span></span> <span></span> <span></span>
+      <span></span> <span></span> <span></span> <span></span> <span></span>
+      <span></span> <span></span> <span></span> <span></span> <span></span>
+      <span></span> <span></span> <span></span> <span></span> <span></span>
+      <span></span> <span></span> <span></span> <span></span> <span></span>
+      <span></span> <span></span> <span></span> <span></span> <span></span>
+      <span></span> <span></span> <span></span> <span></span> <span></span>
+      <span></span> <span></span> <span></span> <span></span> <span></span>
+      <span></span> <span></span> <span></span> <span></span> <span></span>
+      <span></span> <span></span> <span></span> <span></span> <span></span>
+      <span></span> <span></span> <span></span> <span></span> <span></span>
+      <span></span> <span></span> <span></span> <span></span> <span></span>
+      <span></span> <span></span> <span></span> <span></span> <span></span>
+      <span></span> <span></span> <span></span> <span></span> <span></span>
+      <span></span> <span></span> <span></span> <span></span> <span></span>
+      <span></span> <span></span> <span></span> <span></span> <span></span>
+      <span></span> <span></span> <span></span> <span></span> <span></span>
+      <span></span> <span></span> <span></span> <span></span> <span></span>
+      <span></span> <span></span> <span></span> <span></span> <span></span>
+      <span></span> <span></span> <span></span> <span></span> <span></span>
+      <span></span> <span></span> <span></span> <span></span> <span></span>
+      <div className="signin">
+        <div className="content">
+          <h2>Sign In</h2>
+
+          <div className="form">
+            <div className="inputBox">
+              <input
+                type="text "
+                placeholder="Username"
+                onChange={handleName}
+                value={name}
+              />
+            </div>
+
+            <div className="inputBox">
+              <input
+                type="password"
+                onChange={handelPassword}
+                value={password}
+                required
+                placeholder="Password"
+              />
+            </div>
+            <div className="inputBox">
+              <input
+                onChange={handleEmail}
+                value={email}
+                type="email"
+                required
+                placeholder="Email"
+              />
+            </div>
+
+            <div className="  cards inputBox">
+              <p className="message">{message}</p>
+            </div>
+
+            <div className="links">
+              <a href="#">Forgot Password</a> <a href="#">Signup</a>
+            </div>
+
+            <div className="  inputBox">
+              <input type="submit" value="Login" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </form>
+  );
+};
+
+export default Forms;
+//
